eafbfd5f51634e482e4806c1fc3aae2e9b6d720e
Tim Quimbo <tquimbo1@gmail.com>
2021-12-06 21:34:43 -0500

    commit tree ff69757ab68fe96e08e4a7f9cc41d79b9bb9786b

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,59 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+    
+    <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={link}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/movies"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={link}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Movies
+        </NavLink>
+        
+
+        <NavLink
+          to="/direcotrs"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Directors
+        </NavLink>
+
+        <NavLink
+          to="/actors"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Actors
+        </NavLink>
+
+        
     </div>
   );
 };
